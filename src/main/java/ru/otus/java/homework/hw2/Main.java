66a263bd8207682ff2a053911512fb5d49bbd96d
Sernam <wasl@list.ru>
2024-04-25 18:49:46 +0300

    дз Основы языка Java. Часть 2

---
+++ src/main/java/ru/otus/java/homework/hw2/Main.java
@@ -0,0 +1,64 @@
+package ru.otus.java.homework.hw2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        printNTimes(5, "Строка");
+        int[] test = {1, 5 ,6 ,7, 8 ,9};
+        sumOfElements(test);
+        changeElements(2, test);
+        addToElements(11, test);
+        greaterHalf(test);
+
+
+
+    }
+
+    public static void printNTimes(int in, String str){
+        for (int i = 0 ; i < in; i++){
+            System.out.println(str);
+        }
+     }
+
+     public static void sumOfElements(int[] input){
+        int sum = 0;
+        for (int i = 0; i < input.length; i++){
+            if (input[i] > 5){
+                sum += input[i];
+            }
+        }
+         System.out.println(sum);
+     }
+
+     public static void changeElements(int change, int[] input){
+        for (int i =  0; i < input.length; i++){
+            input[i] = change;
+        }
+         //System.out.println(Arrays.toString(input));
+     }
+
+     public static void addToElements(int add, int[] input){
+        for (int i = 0; i < input.length; i++){
+            input[i] += add;
+        }
+         //System.out.println(Arrays.toString(input));
+     }
+
+     public static void greaterHalf(int[] input){
+        int sumRight = 0;
+        int sumLeft = 0;
+        for (int i = 0; i < input.length / 2; i++){
+            sumRight += input[i];
+        }
+        for (int i = input.length / 2; i < input.length; i++){
+            sumLeft += input[i];
+        }
+        if (sumRight > sumLeft){
+            System.out.println("Первая половина больше");
+        } else if (sumRight < sumLeft){
+            System.out.println("Вторая половина больше");
+        } else System.out.println("Суммы равны");
+     }
+
+
+}
